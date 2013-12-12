68d61c64468bfdfd6251208321c1eece5d805641
Atin Sood <atinsood@gmail.com>
2013-12-11 22:39:10 -0500

    added chat page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,7 @@ angular.module('socketioApp', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: 'views/chat.html',
         controller: 'MainCtrl'
       })
       .otherwise({
